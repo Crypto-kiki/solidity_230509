9c45e92c70c2a140f66e3f9fa58e4061e85bca6f
kiki <ejo2123@naver.com>
2023-05-09 11:34:19 +0900

    Mapping

--- 230509.sol
+++ 230509.sol
@@ -8,7 +8,7 @@ n번째 학생을 반환해주는 함수
 n번째 학생의 이름을 반환해주는 함수를 구현하세요.
 */
 
-contract quize {
+contract quiz {
     struct Student {
         string name;
         uint birth;
@@ -27,5 +27,103 @@ contract quize {
 
     function getName(uint _n) public view returns(string memory) {
         return students[_n - 1].name;
+    }
+}
+
+contract quiz2 {
+    // 이름 a, 번호 b, bytes2 c를 담은 구조체 D
+
+    struct D {
+        string a;
+        uint b;
+        bytes2 c;
+    }
+
+    //D형 변수 ddd를 선언하시오.
+    D ddd;
+
+    // ddd에 값을 부여하는 함수를 구현하시오.
+
+    function pushDdd(string memory _a, uint _b, bytes2 _c) public {
+        ddd = D(_a, _b, _c);
+    }
+
+    // D가 들어가는 array D_list를 선언하시오.
+    D[] D_list;
+
+    // D_list 전체를 반환하는 함수, D_list 안에서 n번째 데이터를 반환하는 함수를 각각 구현하시오.
+    function getD() public view returns(D[] memory) {
+        return D_list;
+    }
+
+    function getN(uint _n) public view returns(D memory) {
+        return D_list[_n - 1];
+    }
+
+    function pushD(string memory _a, uint _b, bytes2 _c) public {
+        D_list.push(D(_a, _b, _c));
+    }
+
+}
+
+contract quiz3 {
+    // ABC라는 구조체 안에는 숫자형 a, 문자형 b, 문자형 array c가 들어있다.
+
+    struct ABC {
+        uint a;
+        string b;
+        string[] c;
+    }
+
+    // ABC가 들어가는 array ABCs를 구현하고 ABCs에 ABC를 넣는 함수
+    ABC[] ABCs;
+    function setABC(uint _a, string memory _b, string[] memory _c) public {
+        ABCs.push(ABC(_a, _b, _c));
+    }
+
+    // 특정 ABC를 반환하는 함수
+    function getABC(uint _n) public view returns(ABC memory) {
+        return ABCs[_n - 1];
+    }
+
+    // ABCs 전체를 반환하는 함수
+    function getABCS() public view returns(ABC[] memory) {
+        return ABCs;
+    }
+
+    // 특정 ABC의 c array를 반환받는 함수
+    function getArray_C(uint _n) public view returns(string[] memory) {
+        return ABCs[_n - 1].c;
+    }
+
+    // array인 c 에 값을 녛으려면 ["c", "d", "x" ... ] 이런식으로 넣어야 함.
+
+}
+
+
+contract Mapping {
+    // 맵핑은 검색할 때 유용함. (key - value 쌍)
+    mapping(uint => uint) a;
+    mapping(string => uint) b;
+    mapping(bytes => uint) c;
+
+    function push(string memory _b, uint _bb) public {
+        b[_b] = _bb;
+    }
+
+    function setB(string memory _key, uint _value) public {
+        b[_key] = _value;
+    }
+
+    function getB(string memory _key) public view returns(uint) {
+        return b[_key];
+    }
+
+    function setC(bytes memory _key, uint _value) public {
+        c[_key] = _value;
+    }
+
+    function getC(bytes memory _key) public view returns(uint) {
+        return c[_key];
     }
 }
